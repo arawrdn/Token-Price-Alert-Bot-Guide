284789a507b21f0616fd6babe1bb80169b79e6f6
0xward <aradeawardana97@gmail.com>
2025-10-06 15:13:09 +0700

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  telegramBotToken: "YOUR_TELEGRAM_BOT_TOKEN",
+  telegramChatId: "YOUR_CHAT_ID",
+  discordWebhookUrl: "YOUR_DISCORD_WEBHOOK",
+  tokens: [
+    { symbol: "ETH", threshold: 2000 },
+    { symbol: "BTC", threshold: 40000 }
+  ]
+};
